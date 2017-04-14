36df3d5d3cf3a79a39deefb6e76ef5264f921007
Sonny <sonnybunzz@gmail.com>
2017-04-14 01:17:35 +0000

    4.13.17 update2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
- resources :articles, :path => 'reservations'
+
 
   devise_for :users
-resources :articles do
+resources :articles, :path => 'reservations' do
   resources :comments
 end
  
